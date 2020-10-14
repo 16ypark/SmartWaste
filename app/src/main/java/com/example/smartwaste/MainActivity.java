8269068f227f55dd61bed96da81b9eba2771328a
wldus7660 <wldus7660@naver.com>
2020-10-15 00:50:22 +0900

    feature/my_location

--- app/src/main/java/com/example/smartwaste/MainActivity.java
+++ app/src/main/java/com/example/smartwaste/MainActivity.java
@@ -1,51 +1,138 @@
 package com.example.smartwaste;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import com.naver.maps.map.MapView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.util.Log;
+import android.view.Gravity;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.LocationOverlay;
+import com.naver.maps.map.util.FusedLocationSource;
 
-import android.util.Log;
-import android.view.View;
+public class MainActivity<NMapLocationManager> extends AppCompatActivity
+        implements OnMapReadyCallback, MainFragment.OnNewButtonTappedListener, LocationListener {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private static final String TAG = "MainActivity";
 
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity
-        implements MainFragment.OnNewButtonTappedListener {
     private MapView mapView;
     private FragmentManager fragmentManager;
     private MainFragment fragmentMain;
     private AddFragment fragmentAdd;
     private FragmentTransaction transaction;
+    private FusedLocationSource locationSource;
+    private NaverMap map;
+    private NMapLocationManager mMapLocationManager;
+    private LocationManager locationManager;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         fragmentManager = getSupportFragmentManager();
-
         fragmentMain = new MainFragment();
         fragmentAdd = new AddFragment();
-
         transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frameLayout, fragmentMain).commitAllowingStateLoss();
-
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                map.setLocationTrackingMode(LocationTrackingMode.Face);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mapView.onStart();
+
+        if (hasPermission()) {
+            if (locationManager != null) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 1000, 10, (android.location.LocationListener) this);
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                    this, PERMISSIONS,LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (locationManager != null) {
+            locationManager.removeUpdates((android.location.LocationListener) this);
+        }
+    }
+
+    public void onLocationChanged(Location location) {
+        if (map == null || location == null) {
+            return;
+        }
+        LatLng coord = new LatLng(location);
+        LocationOverlay locationOverlay =map.getLocationOverlay();
+        locationOverlay.setVisible(true);
+        locationOverlay.setPosition(coord);
+        locationOverlay.setBearing(location.getBearing());
+        map.moveCamera(CameraUpdate.scrollTo(coord));
+    }
+
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    public void onProviderEnabled(String provider) {
+    }
+
+    public void onProviderDisabled(String provider) {
+    }
+
+    private boolean hasPermission() {
+        return PermissionChecker.checkSelfPermission(this, PERMISSIONS[0])
+                == PermissionChecker.PERMISSION_GRANTED
+                && PermissionChecker.checkSelfPermission(this, PERMISSIONS[1])
+                == PermissionChecker.PERMISSION_GRANTED;
     }
 
     @Override
@@ -67,12 +154,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
@@ -88,5 +169,19 @@ public class MainActivity extends AppCompatActivity
     public void onNewButtonTapped() {
         transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frameLayout, fragmentAdd).commitAllowingStateLoss();
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+            Log.d( TAG, "onMapReady");
+            this.map = naverMap;
+            naverMap.setLocationSource(locationSource);
+            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            UiSettings uiSettings = naverMap.getUiSettings();
+            uiSettings.setLocationButtonEnabled(true); // 기본값 : false
+            uiSettings.setLogoGravity(Gravity.RIGHT|Gravity.BOTTOM);
+            ActivityCompat.requestPermissions(this, PERMISSIONS, (Integer) LOCATION_PERMISSION_REQUEST_CODE);
+
     }
 }
